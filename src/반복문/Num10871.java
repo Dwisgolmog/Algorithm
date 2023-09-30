9d1e85056daaa07fe76bdfbfbf113b5872ef95b2
Dwisgolmog <rlaehdgus134@naver.com>
2023-09-30 19:05:30 +0900

    버퍼리더와 어레이리스틀 사용하였음 속도:304m 개선필요

---
+++ src/반복문/Num10871.java
@@ -0,0 +1,34 @@
+package 반복문;
+
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Num10871 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<Integer> list = new ArrayList<>();
+        StringTokenizer st;
+        st = new StringTokenizer(bf.readLine());
+
+        int count = Integer.parseInt(st.nextToken());
+        int boolNum  = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(bf.readLine());
+        int num =0;
+        for(int i=0; i<count; i++){
+            num = Integer.parseInt(st.nextToken());
+            if(boolNum>num){
+                list.add(num);
+            }
+        }
+
+        for(int i=0; i<list.size(); i++){
+            System.out.print(list.get(i)+" ");
+        }
+    }
+}
