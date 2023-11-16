db05def4084c91a63f3ce6df7b60d82652b9c1cd
dwisgolmog <rlaehdgus134@naver.com>
2023-11-16 16:05:29 +0900

    27866번,문자와 문자열, 속도:76ms 코드길이:485B charAt()를 사용하여 문자열의 특정 숫자번째의 글자를 추출하였음

--- src/문자열/Num27866.java
+++ src/문자열/Num27866.java
@@ -2,13 +2,15 @@ package 문자열;
 
 import java.lang.*;
 import java.io.*;
-public class Num2743 {
+
+public class Num27866 {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int length = br.readLine().length();
-        bw.write(String.valueOf(length));
+        String str = br.readLine();
+        int count = Integer.parseInt(br.readLine());
+        bw.write(str.charAt(count-1));
         bw.flush();
         bw.close();
     }
