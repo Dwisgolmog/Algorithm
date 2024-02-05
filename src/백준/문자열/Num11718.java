c6bd00b004a85d796110124ce1fe04aa5218aaba
Dwisgolmog <rlaehdgus134@naver.com>
2024-02-05 12:17:52 +0900

    11717번 문제 속도:124ms 코드길이:289B

---
+++ src/백준/문자열/Num11718.java
@@ -0,0 +1,12 @@
+package 문자열;
+
+import java.io.*;
+import java.util.Scanner;
+public class Num11718 {
+    public static void main(String[] args)throws IOException {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            System.out.println(sc.nextLine());
+        }
+    }
+}
