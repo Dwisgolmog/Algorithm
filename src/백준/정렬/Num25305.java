0b37f37a7319a019c89e7d5f90cd21b67fd74a72
Dwisgolmog <rlaehdgus134@naver.com>
2025-01-18 23:36:57 +0900

    25305번 문제 속도:76ms 코드길이:692B
    sort를 할때 굳이 역순 정렬법을 사용하지 않고 students.length - cutStudents로 연산을 하여도 똑같은 결과값이 나올뿐더러 속도는 72ms로 조금더 빠르다.
    다만 이번기회에 자바에서 역순정렬을 할때는 어떻게 할까?에 대해 알게되었다.
    sort의 두번째 파라미터에 Collections.reverseOrder()를 사용하면 된다.
    다만 주의할점이 있다면 배열의 타입이 래퍼클래스만 가능하고 int,double,char,float와 같은 프리미티브 타입은 사용이 불가능하다.

---
+++ src/백준/정렬/Num25305.java
@@ -0,0 +1,22 @@
+package 백준.정렬;
+
+import java.io.*;
+import java.util.*;
+
+public class Num25305 {
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Integer[] students = new Integer[Integer.parseInt(st.nextToken())];
+        int cutStudents = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<students.length; i++){
+            students[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(students,Collections.reverseOrder());
+
+        System.out.println(students[cutStudents-1]);
+    }
+}
