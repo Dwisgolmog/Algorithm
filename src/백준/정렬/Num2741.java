c2a6e5b1fc8d8b3b694f0435dbb5db6c9330fd9a
Dwisgolmog <rlaehdgus134@naver.com>
2025-01-19 22:41:57 +0900

    2751번 문제 속도:1216ms 코드길이:613B
    해당 문제는 O(n^2)이 되면 시간초과가 걸리는 문제이다.
    때문에 단순히 Arrays.sort()로 풀게 되면 최악의 경우 O(n^2)이 되기때문에 시간초과로 문제가 풀리지 않게 된다.
    간단한 방법은 Collections.sort()를 활용해서 풀면 되는데 이는 프리미티브 배열이 아닌 List 계열의 자료구조를 사용하여 정렬해야 한다.
    기존의 Arrays.sort()는 dual-pivot Quicksort 알고리즘을 사용한다면, Collections.sort()는 Timsort 알고리즘을 사용한다. Timsort는 합병 정렬을 기반으로 구현하되, 일정 크기 이하의 부분 리스트에 대해서는 이진 삽입 정렬이 사용되는 혼용 하이브리드 정렬이다. 시간복잡도는 O(n) ~ O(nlogn)을 보장한다.

---
+++ src/백준/정렬/Num2741.java
@@ -0,0 +1,25 @@
+package 백준.정렬;
+
+import java.io.*;
+import java.util.*;
+
+public class Num2741 {
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int count = Integer.parseInt(br.readLine());
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i=0; i<count; i++){
+            list.add(Integer.parseInt(br.readLine()));
+        }
+
+        Collections.sort(list);
+
+        for(int num: list){
+            sb.append(num).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
