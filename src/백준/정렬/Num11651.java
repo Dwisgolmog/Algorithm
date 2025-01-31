5fb6c3a4173e634580bd9d5da1b780088fe31490
Dwisgolmog <rlaehdgus134@naver.com>
2025-01-31 23:58:00 +0900

    11651번 문제 속도:612ms 코드길이:912B

---
+++ src/백준/정렬/Num11651.java
@@ -0,0 +1,31 @@
+package 백준.정렬;
+
+import java.io.*;
+import java.util.*;
+
+public class Num11651 {
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int count = Integer.parseInt(br.readLine());
+        int[][] result = new int[count][2];
+
+        for(int i=0; i<count; i++){
+            st = new StringTokenizer(br.readLine());
+            result[i][0] = Integer.parseInt(st.nextToken());
+            result[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(result,(x,y) ->{
+           if(x[1] == y[1]) return Integer.compare(x[0],y[0]);
+           return Integer.compare(x[1],y[1]);
+        });
+
+        for(int[] num: result){
+            sb.append(num[0]).append(" ").append(num[1]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
