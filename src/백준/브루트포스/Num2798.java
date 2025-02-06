b00a4955758b7cbe272057ddab747f2fa0d27282
Dwisgolmog <rlaehdgus134@naver.com>
2025-02-07 02:55:18 +0900

    2798번 문제 속도:68ms 코드길이:977B

---
+++ src/백준/브루트포스/Num2798.java
@@ -0,0 +1,34 @@
+package 백준.브루트포스;
+
+import java.io.*;
+import java.util.*;
+
+public class Num2798 {
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int count = Integer.parseInt(st.nextToken());
+        int limit = Integer.parseInt(st.nextToken());
+        int[] card = new int[count];
+        int result = 0;
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<count; i++){
+            card[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i=0; i<count-2; i++){
+            for(int j=i+1; j<count-1; j++){
+                for(int c=j+1; c<count; c++){
+                    int sum = card[i]+card[j]+card[c];
+                    if(sum <= limit) {
+                        if(sum > result) result = sum;
+                    }
+                }
+            }
+        }
+
+        System.out.println(result); 
+    }
+}
