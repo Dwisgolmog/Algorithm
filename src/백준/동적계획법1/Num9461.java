9cc59b6134e944d07e65c3d84450d771a9cf7f7d
Dwisgolmog <rlaehdgus134@naver.com>
2025-05-28 11:19:27 +0900

    9461번 문제 속도:64ms 코드길이:799B
    피보나치와 비슷한 점화식을 가진다.
    추가로 초기값을 전보다 더 많이 설정해주면 피보나치와 동일하다.

---
+++ src/백준/동적계획법1/Num9461.java
@@ -0,0 +1,32 @@
+package 백준.동적계획법1;
+
+import java.io.*;
+
+public class Num9461 {
+
+    static long[] memo;
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int count = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<count; i++){
+            int num = Integer.parseInt(br.readLine());
+            memo = new long[101];
+            sb.append(dp(num)).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    public static long dp(int num){
+        memo[1] = memo[2] = memo[3] = 1;
+        memo[4] = memo[5] = 2;
+        if(num <= 5) return memo[num];
+
+        for(int i=6; i<=num; i++){
+            memo[i] = memo[i-1] + memo[i-5];
+        }
+
+        return memo[num];
+    }
+}
