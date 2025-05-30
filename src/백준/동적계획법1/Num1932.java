37bebb3ddbb6113f8b2300d5c564af3eb7ec7bea
Dwisgolmog <rlaehdgus134@naver.com>
2025-05-30 10:35:41 +0900

    1932번 문제 속도:212ms 코드길이:910B
    이전의 Num1149 문제와 동일한 점화식을 가진다.
    단, 이전에는 최솟값을 구했다면 이번에는 최댓값을 구하면 된다.

---
+++ src/백준/동적계획법1/Num1932.java
@@ -0,0 +1,34 @@
+package 백준.동적계획법1;
+
+import java.io.*;
+import java.util.*;
+
+public class Num1932 {
+    static int[][] map;
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int count = Integer.parseInt(br.readLine());
+        map = new int[count+1][count+1];
+
+        StringTokenizer st;
+        for(int i=1; i<=count; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=1; j<i+1; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for(int i=2; i<=count; i++){
+            for(int j=1; j<i+1; j++){
+                map[i][j] += Math.max(map[i-1][j-1],map[i-1][j]);
+            }
+        }
+
+        int result = 0;
+        for(int i=0; i<=count; i++){
+            if(result < map[count][i]) result = map[count][i];
+        }
+
+        System.out.println(result);
+    }
+}
