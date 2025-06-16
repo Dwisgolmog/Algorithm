e794216049e83bfa298608c108f17ca4da85ed7a
Dwisgolmog <rlaehdgus134@naver.com>
2025-06-16 21:46:17 +0900

    12865번 문제 속도:140ms 코드길이:1012B
    LCS와 동일하게 2차원표로 나타내어 규칙을 찾아 풀이한다.
    표의 x축은 0부터 받을 수 있는 최대 무게, y축은 입력받는 배열의 수로 나타낸다.

---
+++ src/백준/단계별로풀이/브루트포스/Num12865.java
@@ -0,0 +1,36 @@
+package 백준.단계별로풀이.브루트포스;
+
+import java.io.*;
+import java.util.*;
+
+public class Num12865 {
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] W = new int[N+1];
+        int[] V = new int[N+1];
+        int[][] dp = new int[N + 1][K + 1];
+
+        for(int i=1; i<=N; i++){
+            st = new StringTokenizer(br.readLine());
+            W[i] = Integer.parseInt(st.nextToken());
+            V[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i=1; i<=N; i++){
+            for(int j=1; j<=K; j++){
+                if(W[i] > j){
+                    dp[i][j] = dp[i-1][j];
+                }else{
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i-1][j-W[i]] + V[i]);
+                }
+            }
+        }
+
+        System.out.println(dp[N][K]);
+    }
+}
