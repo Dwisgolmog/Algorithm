72b5c63a794dbac0d6638be92d44e20138d3ba69
Dwisgolmog <rlaehdgus134@naver.com>
2025-08-13 23:47:48 +0900

    17626번 문제 속도:96ms 코드길이:492B

---
+++ src/백준/solvedac/class3/Num17626.java
@@ -0,0 +1,20 @@
+package 백준.solvedac.class3;
+
+import java.io.*;
+
+public class Num17626 {
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        int[] dp = new int[num+1];
+
+        for(int i=1; i<=num; i++){
+            dp[i] = i;
+            for(int j=1; j*j<=i; j++){
+                dp[i] = Math.min(dp[i],dp[i-j*j] + 1);
+            }
+        }
+
+        System.out.println(dp[num]);
+    }
+}
